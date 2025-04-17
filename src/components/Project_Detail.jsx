6a07db420aa4e817c5f8decb11929d307e2aaaa5
amenmarzouk <marzoukamen10@gmail.com>
2025-04-17 18:39:49 +0100

    tasks

--- src/components/Project_Detail.jsx
+++ src/components/Project_Detail.jsx
@@ -5,14 +5,12 @@ import { database } from "../../configuration";
 import { get, push, ref, remove, update } from "firebase/database";
 import { projcontext } from "../context/proj";
 import toast, { Toaster } from "react-hot-toast";
+import Tasks from "./Tasks";
 
 const ProjectDetail = () => {
   const nav = useNavigate();
   const projcon = useContext(projcontext);
   const [Project, setProject] = useState({});
-  const taskref = useRef();
-  console.log("fff", taskref.current);
-
   const params = useParams();
   const getoneproject = async () => {
     const db = ref(database, `projects/${params.id}`);
@@ -29,38 +27,8 @@ const ProjectDetail = () => {
       console.error("Erreur :", error);
     }
   };
-  const add_handler = (event) => {
-    event.preventDefault();
-    const dbRef = ref(database, `projects/${params.id}`);
-    if (Project.Tasks) {
-      update(dbRef, { Tasks: [...Project.Tasks, taskref.current] })
-        .then(() => {
-          console.log("Données envoyées avec succès");
-          getoneproject();
-        })
-        .catch((error) => console.error("Erreur :", error));
-    } else {
-      update(dbRef, { Tasks: [taskref.current] })
-        .then(() => {
-          console.log("Données envoyées avec succès");
-          getoneproject();
-        })
-        .catch((error) => console.error("Erreur :", error));
-    }
-  };
-  const clear_handler = (id) => {
-    const dbRef = ref(database, `projects/${params.id}`);
-
-    const newTasks = Project.Tasks.filter((task) => task.id !== id);
-    console.log("eze", Project.Tasks);
-
-    update(dbRef, { Tasks: newTasks })
-      .then(() => {
-        console.log("Task supprimée avec succès");
-        getoneproject();
-      })
-      .catch((error) => console.error("Erreur :", error));
-  };
+ 
+ 
   const delete_handler = () => {
     const dbRef = ref(database, `projects/${params.id}`);
 
@@ -76,7 +44,7 @@ const ProjectDetail = () => {
       })
       .catch((error) => console.error("Erreur :", error));
   };
-  const genid = Math.random().toString(36);
+
 
   useEffect(() => {
     getoneproject();
@@ -98,36 +66,7 @@ const ProjectDetail = () => {
         <div className="text-black ">{Project && Project.Date}</div>
         <div className="text-black ">{Project && Project.Description}</div>
         <div className="bg-[#D6D3D1] rounded-md h-[3px] w-120 ml-auto mr-auto"></div>
-        <div className="">Tasks</div>
-        <form className="flex gap-5" onSubmit={add_handler}>
-          <input
-            type="text"
-            placeholder="Enter a TASK"
-            className="h-10 w-100 bg-gray-200 rounded-md pl-2"
-            onChange={(e) => {
-              taskref.current = { id: genid, name: e.target.value };
-            }}
-            required={true}
-          />
-          <button
-            type="submit"
-            className="bg-gray-500 rounded-md py-2 px-1 cursor-pointer"
-          >
-            Add Task
-          </button>
-        </form>
-        {Project.Tasks &&
-          Project.Tasks.map((task) => (
-            <div
-              key={task.id}
-              className="bg-gray-200 rounded-md p-2 flex justify-between"
-            >
-              <div className="">{task.name}</div>
-              <button className="" onClick={() => clear_handler(task.id)}>
-                clear
-              </button>
-            </div>
-          ))}
+        <Tasks id={params.id} getone={getoneproject} project={Project}/>
       </div>
     </div>
   );
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,80 @@
+import React, { useRef } from 'react';
+
+import { database } from '../../configuration';
+import { ref, update } from 'firebase/database';
+
+const Tasks = (props) => {
+
+    const taskref = useRef({id: "", name: ""});
+
+    const clear_handler = (id) => {
+        const dbRef = ref(database, `projects/${props.id}`);
+    
+        const newTasks = props.project.Tasks.filter((task) => task.id !== id);
+
+    
+        update(dbRef, { Tasks: newTasks })
+          .then(() => {
+            console.log("Task supprimée avec succès");
+            props.getone();
+          })
+          .catch((error) => console.error("Erreur :", error));
+      };
+      const add_handler = (event) => {
+        const genid = Math.random().toString(36);
+        taskref.current.id = genid;
+        event.preventDefault();
+        const dbRef = ref(database, `projects/${props.id}`);
+        if (props.project.Tasks) {
+          update(dbRef, { Tasks: [...props.project.Tasks, taskref.current] })
+            .then(() => {
+              console.log("Données envoyées avec succès");
+              props.getone();
+            })
+            .catch((error) => console.error("Erreur :", error));
+        } else {
+          update(dbRef, { Tasks: [taskref.current] })
+            .then(() => {
+              console.log("Données envoyées avec succès");
+             props.getone();
+            })
+            .catch((error) => console.error("Erreur :", error));
+        }
+      };
+    return (
+        <div>
+            <div className="">Tasks</div>
+        <form className="flex gap-5" onSubmit={add_handler}>
+          <input
+            type="text"
+            placeholder="Enter a TASK"
+            className="h-10 w-100 bg-gray-200 rounded-md pl-2"
+            onChange={(e) => {
+              taskref.current = {name: e.target.value };
+            }}
+            required={true}
+          />
+          <button
+            type="submit"
+            className="bg-gray-500 rounded-md py-2 px-1 cursor-pointer"
+          >
+            Add Task
+          </button>
+        </form>
+        {props.project.Tasks &&
+          props.project.Tasks.map((task) => (
+            <div
+              key={task.id}
+              className="bg-gray-200 rounded-md p-2 flex justify-between"
+            >
+              <div className="">{task.name}</div>
+              <button className="" onClick={() => clear_handler(task.id)}>
+                clear
+              </button>
+            </div>
+          ))}
+        </div>
+    );
+}
+
+export default Tasks;
